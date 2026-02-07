ccde39c47c194a5660cd0f41fac29a89ae84667a
Bit Wise <troho2003@gmail.com>
2026-02-06 20:41:16 -0800

    actually fix click event triggering, also added broken ui implementation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import {
 	Graphics,
 	extensions,
 	CullerPlugin,
+	Assets,
+	Sprite,
 } from "pixi.js";
 import { Viewport } from "pixi-viewport";
 import { Tile } from "./Tile";
@@ -43,8 +45,11 @@ import { CheckBox, RadioGroup } from "@pixi/ui";
 			tile.gfx.interactive = true;
 			tile.gfx.onpointertap = () => {
 				if (cameraMoving) return;
-				defender.hex = tile.hex;
-				updateLOS(tiles, attacker, defender);
+				if(menu.selected === 0)
+				{
+					defender.hex = tile.hex;
+					updateLOS(tiles, attacker, defender);
+				}
 			};
 			main.addChild(tile.gfx);
 			tiles.push(tile);
@@ -69,30 +74,51 @@ import { CheckBox, RadioGroup } from "@pixi/ui";
 	}
 
 	//create UI
+	const scale = 0.2;
+	const cursorTex = await Assets.load('assets/cursor.png');
+	const treeTex = await Assets.load('assets/tree.png');
 	const menu = new RadioGroup({
 		items: [
 			new CheckBox({
 				style: {
-					unchecked: `switch_off.png`,
-					checked: `switch_on.png`,
+					unchecked: new Sprite({
+						scale:scale,
+						texture: cursorTex,
+						tint: 0xffffff,
+					}),
+					checked: new Sprite({
+						scale:scale,
+						texture: cursorTex,
+						tint: 0xffffff,
+					}),
 				},
 			}),
 			new CheckBox({
 				style: {
-					unchecked: `switch_off.png`,
-					checked: `switch_on.png`,
-				},
-			}),
-			new CheckBox({
-				style: {
-					unchecked: `switch_off.png`,
-					checked: `switch_on.png`,
+					unchecked: new Sprite({
+						scale:scale,
+						texture: treeTex,
+						tint: 0xffffff,
+					}),
+					checked: new Sprite({
+						scale:scale,
+						texture: treeTex,
+						tint: 0x555555,
+					}),
 				},
 			}),
 		],
 		type: "vertical",
 		elementsMargin: 8,
 	});
+
+	if(menu.innerView){
+		menu.innerView.padding = 10;
+	}
+	menu.interactive = true;
+	menu.onChange.connect((i) => {
+	})
+
 	//create viewport
 	const bounds = main.getBounds();
 	const width = bounds.width - layout.size.x * 2;
@@ -121,35 +147,29 @@ import { CheckBox, RadioGroup } from "@pixi/ui";
 
 	//prevent click event during viewport movement
 	let cameraMoving = false;
-	let moveEndTimer: number | null = null;
-	const MOVE_IDLE_DELAY = 50; // ms after last movement to consider stable
-
+	
 	viewport.on("moved", () => {
 		cameraMoving = true;
-
-		if (moveEndTimer !== null) {
-			clearTimeout(moveEndTimer);
-		}
-
-		moveEndTimer = window.setTimeout(() => {
+	});
+	viewport.on("pointerup", () => {
+		requestAnimationFrame(() => {
 			cameraMoving = false;
-			moveEndTimer = null;
-		}, MOVE_IDLE_DELAY);
+		});
 	});
 
 	//update
 	app.ticker.maxFPS = 30;
 	app.ticker.add(() => {
+		defender.update();
 		updateLine();
 		tiles.forEach((tile) => {
 			tile.update();
 		});
-		defender.update();
 	});
 
 	//define hierarchy
 	document.body.appendChild(app.canvas);
-	app.stage.addChild(menu);
 	app.stage.addChild(viewport);
+	app.stage.addChild(menu);
 	viewport.addChild(main);
 })();
